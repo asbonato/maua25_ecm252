85722a787dd72737798463e5833c82d9ce4ddc8e
asbonato <asbonato@gmail.com>
2025-03-08 12:44:54 -0300

    apostila04(react_props): componente Feedback - apostila finalizada

--- react/react-componentes-props/src/App.jsx
+++ react/react-componentes-props/src/App.jsx
@@ -2,8 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 import Pedido from './Pedido'
 import Cartao from './Cartao'
+import Feedback from './Feedback'
 
 const App = () => {
+  const textoOK = "Já chegou"
+  const textoNOK = "Ainda não chegou"
+  const funcaoOK = () => alert("Agrademos a confirmação")
+  const funcaoNOK = () => alert("Verificaremos o ocorrido")
+  const componenteFeedback = <Feedback textoOK={textoOK} funcaoOK={funcaoOK} textoNOK={textoNOK} funcaoNOK={funcaoNOK}/>
+  
   return (
     //container principal
     <div className='container border rounded mt-2'>
@@ -17,12 +24,14 @@ const App = () => {
         <div className='col-sm-8 col-md-6 m2'>
           <Cartao cabecalho="22/04/2021">
             <Pedido icone="fas fa-hdd fa-2x" titulo="SSD" descricao="SSD Kingston A400 - SATA"/>
+            {componenteFeedback}
           </Cartao>
           </div>
         {/* controle de colunas para responsividade */}
         <div className='col-sm-8 col-md-6 m2'>
           <Cartao cabecalho="20/04/2021">
             <Pedido icone="fas fa-book fa-2x" titulo="Livro" descricao="Concrete Mathematics - Donald Knuth"/>
+            {componenteFeedback}
           </Cartao>
         </div>
       </div>
@@ -32,12 +41,14 @@ const App = () => {
         <div className='col-sm-8 col-md-6 m2'>
           <Cartao cabecalho="21/01/2021">
             <Pedido icone="fas fa-laptop fa-2x" titulo="Notebook" descricao="Notebook Dell - 8Gb - i5"/>
+            {componenteFeedback}
           </Cartao>
         </div>
         {/* controle de colunas para responsividade */}
         <div className='col-sm-8 col-md-6 m2'>
           <Cartao cabecalho="08/03/2025">
             <Pedido icone="fas fa-mobile fa-2x" titulo="Celular Galaxy S25" descricao="256Gb - 12Gb RAM - Câmera Tripla - Tela 6 pol"/>
+            {componenteFeedback}
           </Cartao>
         </div>
       </div>     
