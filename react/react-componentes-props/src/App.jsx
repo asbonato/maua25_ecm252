7144ff21cf78765c8765a6947814ca859d6b2b65
asbonato <asbonato@gmail.com>
2025-03-08 11:20:40 -0300

    apostila04(react_props): refat - componente Pedido

--- react/react-componentes-props/src/App.jsx
+++ react/react-componentes-props/src/App.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import Pedido from './Pedido'
 
 const App = () => {
   return (
@@ -13,66 +14,21 @@ const App = () => {
       <div className='row'>
         {/* controle de colunas para responsividade */}
         <div className='col-sm-8 col-md-6 m2'>
-          {/* cartão */}
-          <div className='card'>
-            {/* cabeçalho do cartão */}
-            <div className='card-header text-muted'>22/04/2021</div>
-            {/* corpo do cartão */}
-            <div className='card-body d-flex'>
-              <div className='d-flex align-items-center'>
-                <i className='fas fa-hdd fa-2x'></i>
-              </div>
-              {/* flex-grow 1: tomar espaço remanescente */}
-              <div className='flex-grow-1 ms-2 border'>
-                <h4 className='text-center'>SSD</h4>
-                <p className='text-center'>SSD Kingston A400-SATA</p>
-              </div>
-            </div>
-          </div>
+        <Pedido data="22/04/2021" icone="fas fa-hdd fa-2x" titulo="SSD" descricao="SSD Kingston A400 - SATA"/>
         </div>
       </div>
       {/* linha para o segundo pedido */}
       <div className='row'>
         {/* controle de colunas para responsividade */}
         <div className='col-sm-8 col-md-6 m2'>
-          {/* cartão */}
-          <div className='card'>
-            {/* cabeçalho do cartão */}
-            <div className='card-header text-muted'>20/04/2021</div>
-            {/* corpo do cartão */}
-            <div className='card-body d-flex'>
-              <div className='d-flex align-items-center'>
-                <i className='fas fa-book fa-2x'></i>
-              </div>
-              {/* flex-grow 1: tomar espaço remanescente */}
-              <div className='flex-grow-1 ms-2 border'>
-                <h4 className='text-center'>Livro</h4>
-                <p className='text-center'>Concrete Mathematics - Donald Knuth</p>
-              </div>
-            </div>
-          </div>
+          <Pedido data="20/04/2021" icone="fas fa-book fa-2x" titulo="Livro" descricao="Concrete Mathematics - Donald Knuth"/>
         </div>
       </div>
       {/* linha para o terceiro pedido */}
       <div className='row'>
         {/* controle de colunas para responsividade */}
         <div className='col-sm-8 col-md-6 m2'>
-          {/* cartão */}
-          <div className='card'>
-            {/* cabeçalho do cartão */}
-            <div className='card-header text-muted'>21/01/2021</div>
-            {/* corpo do cartão */}
-            <div className='card-body d-flex'>
-              <div className='d-flex align-items-center'>
-                <i className='fas fa-laptop fa-2x'></i>
-              </div>
-              {/* flex-grow 1: tomar espaço remanescente */}
-              <div className='flex-grow-1 ms-2 border'>
-                <h4 className='text-center'>Notebook</h4>
-                <p className='text-center'>Notebook Dell - 8Gb - i5</p>
-              </div>
-            </div>
-          </div>
+          <Pedido data="21/01/2021" icone="fas fa-laptop fa-2x" titulo="Notebook" descricao="Notebook Dell - 8Gb - i5"/>
         </div>
       </div>      
     </div>
