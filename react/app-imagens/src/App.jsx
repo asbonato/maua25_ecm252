b7602a92f8cce635d73529e97e3bca7135bfdc73
asbonato <asbonato@gmail.com>
2025-04-26 12:47:30 -0300

    apostila07(forms_events_lists): aula 26/4 - seção 2.10 fazendo reqs     retomar em 2.11 componentes imagens - pag. 24

--- react/app-imagens/src/App.jsx
+++ react/app-imagens/src/App.jsx
@@ -5,15 +5,27 @@ import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 import 'primereact/resources/themes/bootstrap4-light-purple/theme.css'
 import Busca from './components/Busca'
+import { createClient } from 'pexels'
 
 export default class App extends Component {
+  state = {pics: []}
+  pexelsClient = null
+  PEXELS_KEY = import.meta.env.VITE_PEXELS_KEY
 
   onBuscaRealizada = (termo) => {
-    console.log(termo)
+    this.pexelsClient.photos.search({
+      query: termo
+    })
+    .then(pics => this.setState({pics: pics.photos}))
   }
 
+  componentDidMount(){
+    this.pexelsClient = createClient(this.PEXELS_KEY)
+  }
+
+
+
   render(){
-    console.log(import.meta.env.VITE_PEXELS_KEY)
     return(
       <div className='grid justify-content-center m-auto w-9 border-round border-1 border-400'>
         <div className="col-12">
@@ -21,6 +33,15 @@ export default class App extends Component {
         </div>
         <div className="col-8">
           <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+        <div className="col-8">
+          {
+            this.state.pics.map((pic, key) => (
+              <div key={key}>
+                <img src={pic.src.small}/>
+              </div>
+            ))
+          }
         </div>
       </div>
     )
