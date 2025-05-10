7d2698072c0388d1f56bbfeb9634ead117d216b7
asbonato <asbonato@gmail.com>
2025-05-10 10:45:41 -0300

    apostila07(forms_events_lists): aula 10/5 - seção 2.12 pexels logo         retomar em 2.13 req http com axios - pag. 29

---
+++ react/app-imagens/src/components/PexelsLogo.jsx
@@ -0,0 +1,13 @@
+const PexelsLogo = () => {
+    return (
+        <div>
+            {/* targe=blank abre a página em nova guia */}
+            <a href='https://www.pexels.com' target="blank">
+                <img width={75}
+                     src='https://images.pexels.com/lib/api/pexels.png'/>
+            </a>
+        </div>
+    )
+}
+
+export default PexelsLogo
