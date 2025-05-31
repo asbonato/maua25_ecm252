62b906a3b6459278e9df7daf183e331730313203
asbonato <asbonato@gmail.com>
2025-05-31 10:44:41 -0300

    apostila08(hooks): aula 31/5 - apostila finalizada

--- react/hooks-react/src/components/Busca.jsx
+++ react/hooks-react/src/components/Busca.jsx
@@ -1,13 +1,22 @@
+/* eslint-disable no-unused-vars */
 import { IconField } from 'primereact/iconfield'
 import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import striptags from 'striptags'
+//import striptags from 'striptags'
+import { Button } from 'primereact/button'
 
 const Busca = () => {
-    const [termoDeBusca, setTermoDeBusca] = useState('')
+    const [termoDeBusca, setTermoDeBusca] = useState('React')
     const [resultados, setResultados] = useState([])
+
+    // useEffect(() => {
+    //     console.log("Causando efeito colateral")
+    //     return () => {
+    //         console.log('Posso limpar algo deixado para trás...')
+    //     }
+    // })
     useEffect(() => {
         //definimos uma função
         const fazBusca = async () => {
@@ -23,9 +32,17 @@ const Busca = () => {
             })
             setResultados(data.query.search)
         }
-        //e chamamos a seguir
-        if (termoDeBusca)
+        if (termoDeBusca && !resultados.length){
             fazBusca()
+        } else {
+            const timeoutId = setTimeout(() => {
+                if (termoDeBusca)
+                    fazBusca()
+            }, 1000)
+            return () => {
+                clearTimeout(timeoutId)
+            }
+        }
     }, [termoDeBusca])
 
     return(
@@ -48,10 +65,18 @@ const Busca = () => {
                             //borda, padding e ajuste textual
                             className='border-bottom border border-1 border-400 p-2 text-center font-bold'>
                             {resultado.title}
+                            <span>
+                                <Button 
+                                    icon="pi pi-send"
+                                    className="ml-2 p-button-rounded p-button-secondary"
+                                    onClick={()=>
+                                        window.open(`https://en.wikipedia.org?curid=${resultado.pageid}`)
+                                    }
+                                />
+                            </span>
                         </div>
                         {/* padding */}
                         <div className="p-2">
-                            <p>{`https://en.wikipedia.org?curid=${resultado.pageid}`}</p>
                             <spam dangerouslySetInnerHTML={{__html: resultado.snippet}}></spam>
                         </div>
                     </div>
